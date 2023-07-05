46cbfe386b10709564bd30a5499ded0b61adbdb1
Tamdepzaiahihi <thanhtam041001@gmail.com>
2023-07-05 13:43:29 +0700

    commit 2

--- src/componenets/Footer/index.tsx
+++ src/componenets/Footer/index.tsx
@@ -90,7 +90,7 @@ function Footer() {
         <a href="https://twitter.com/knyttneve" rel="noopener noreferrer" className="text-gray-600 ml-1" target="_blank">@knyttneve</a>
       </p>
       <span className="inline-flex sm:ml-auto sm:mt-0 mt-2 justify-center sm:justify-start">
-        <a className="text-gray-500">
+        <a className="text-gray-500" href= "https://www.facebook.com/tam410/">
           <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-5 h-5" viewBox="0 0 24 24">
             <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
           </svg>
@@ -104,6 +104,7 @@ function Footer() {
           <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-5 h-5" viewBox="0 0 24 24">
             <rect width="20" height="20" x="2" y="2" rx="5" ry="5"></rect>
             <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"></path>
+
           </svg>
         </a>
         <a className="ml-3 text-gray-500">
